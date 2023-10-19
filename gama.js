d0063d7a449e30d0c64c2e0d42bd44bb82f5322a
Wadim02 <147058574+Wadim02@users.noreply.github.com>
2023-10-19 14:38:53 +0300

    Update gama.js

--- gama.js
+++ gama.js
@@ -13,7 +13,7 @@ document.getElementById("c").innerHTML = "gamma = "+gamma;
 const canvas = document.getElementById("canvas");
 const context = canvas.getContext("2d");
 var centru = {x:canvas.width/2, y:canvas.height/2}
-var latura_patrat = 50+gama;
+var latura_patrat = 50+gamma;
   
 context.clearRect(0,0,canvas.width, canvas.height);
 context.fillStyle = "#6A0888";
